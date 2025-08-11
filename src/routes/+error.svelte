08417aa8b4bca484bf7e2261363c2e2691d7cfe4
Mohammad Ariffansyah <harimato9@gmail.com>
2025-08-11 10:15:21 +0700

    Feat: technologies and new projects

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,17 +1,23 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
 <section class="flex min-h-screen flex-col items-center justify-center px-4">
 	<div
 		class="my-6 flex w-full flex-col items-start justify-center gap-4 rounded-lg p-6 shadow-[0_0_10px_rgba(255,255,255,0.15)] transition duration-300 ease-in-out hover:scale-105 hover:shadow-[0_0_25px_rgba(255,255,255,0.3)] md:mx-auto md:w-1/3"
 	>
 		<div class="flex w-full flex-row items-center justify-between text-white">
 			<div class="flex flex-col">
-				<h1 class="text-5xl font-bold text-red-400">404</h1>
-				<p class="mt-1 text-lg text-gray-400">Page Not Found</p>
+				<h1 class="text-5xl font-bold text-red-400">{$page.status}</h1>
+				<p class="mt-1 text-lg text-gray-400">
+					{$page.error?.message || 'The page you are looking for does not exist.'}
+				</p>
 			</div>
 		</div>
 		<div class="mt-4">
 			<p class="text-justify text-gray-300">
-				Sorry, the page you’re looking for doesn’t exist or has been moved.<br />
-				But don’t worry, let’s get you back home!
+				An error occurred while processing your request. Please try again later or contact support
+				if the problem persists.
 			</p>
 		</div>
 		<a
