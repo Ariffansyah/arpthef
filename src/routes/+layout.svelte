8fe41fa8027908d45e2d65168bc09eb280e7caa2
Mohammad Ariffansyah <harimato9@gmail.com>
2025-08-29 09:09:32 +0700

    Feat: vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,10 @@
 	import Particles from '$lib/components/Particles.svelte';
 	import '../app.css';
 	import AmongUsCursor from '$lib/components/Amogus.svelte';
+	import { dev } from '$app/environment';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit';
+
+	injectAnalytics({ mode: dev ? 'development' : 'production' });
 
 	let targetX = 0,
 		targetY = 0;
